8716c1fb25aa1107d9c74854b9c6537b05c2ab16
Nianres <83022991+nn1rae@users.noreply.github.com>
2023-06-20 14:00:28 +0200

    finaly oop

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from os import getenv
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from configparser import ConfigParser
-import ai_api
+from AIBackEndAPI import BackEndAI
+import time
 
 import keyboard
 
-
+ai = BackEndAI()
 config = ConfigParser()
 
 
@@ -18,28 +19,39 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=getenv("API_TOKEN"))
 dp = Dispatcher(bot)
 
-
-    
+@dp.message_handler(commands=['help'])
+async def message_handler(message: types.Message):
+    text = """Generate image /gen [prompt]
+set steps /steps (steps)
+set cfg scale /cfg (cfg scale)
+set resolution /res (width) (height)
+set model /models
+get corrent configuration /config
+    """
+    await message.answer(text)
+    await bot.delete_message(message.chat.id, message.message_id)
     
 @dp.message_handler(commands=['start'])
 async def message_handler(message: types.Message):
     await message.answer("Successfull")
     
     
+    
 @dp.message_handler(commands=['gen'])
 async def message_handler(message: types.Message):
     prompt = "".join(message.get_args())
     logging.info(f"{message.from_user.full_name} /gen {prompt}")
-    await bot.send_photo(message.chat.id, ai_api.generate_image(prompt), reply_markup=keyboard.generate([["try again", "redo:" + prompt]]))
+    await bot.send_photo(message.chat.id, ai.generate_image(prompt), reply_markup=keyboard.generate([["Reuse prompt", "redo:" + prompt], ["Prompt", "prompt:" + prompt]]))
+    await bot.delete_message(message.chat.id, message.message_id)
     
     
 @dp.message_handler(commands=['steps'])
 async def message_handler(message: types.Message):
     args = message.get_args().split(" ")
     if len(args) == 1 and args[0].isdigit():
-        ai_api.set_steps(args[0])
+        ai.set_steps(args[0])
         logging.info(f"{message.from_user.full_name} set steps to {args[0]}")
-        await bot.send_message(message.chat.id, "success")
+        await bot.delete_message(message.chat.id, message.message_id)
     else:
         
         logging.warning(f"{message.from_user.full_name} /steps {args}")
@@ -49,9 +61,9 @@ async def message_handler(message: types.Message):
 async def message_handler(message: types.Message):
     args = message.get_args().split(" ")
     if len(args) == 1 and args[0].isdigit():
-        ai_api.set_cfg(args[0])
+        ai.set_cfg(args[0])
         logging.info(f"{message.from_user.full_name} set cfg to {args[0]}")
-        await bot.send_message(message.chat.id, "success")
+        await bot.delete_message(message.chat.id, message.message_id)
     else:
         
         logging.warning(f"{message.from_user.full_name} /cfg {args}")
@@ -62,27 +74,37 @@ async def message_handler(message: types.Message):
     try: 
         width, height = message.get_args().split(maxsplit=1)
         if width.isdigit() and height.isdigit():
-            ai_api.set_res(width, height)
+            ai.set_res(width, height)
             logging.info(f"{message.from_user.full_name} set res to {message.get_args()}")
-            await bot.send_message(message.chat.id, "success")
+            await bot.delete_message(message.chat.id, message.message_id)
         else:
             
             logging.warning(f"{message.from_user.full_name} /res {message.get_args()}")
             await bot.send_message(message.chat.id, "Wrong fromat, this will be reported")
     except:
         pass
+
+@dp.message_handler(commands=['models'])
+async def message_handler(message: types.Message):
+    await bot.delete_message(message.chat.id, message.message_id)
+    await message.answer("Available models:", reply_markup=keyboard.generate([[model_name, "change_model:" + model_name] for model_name in ai.get_modes()]))
     
 @dp.message_handler(commands=['config'])
 async def message_handler(message: types.Message):
-    await bot.send_message(message.chat.id, ai_api.get_config())
+    await bot.delete_message(message.chat.id, message.message_id)
+    await bot.send_message(message.chat.id, ai.get_config())
         
 @dp.callback_query_handler()
 async def claabackfunc(callback: types.CallbackQuery):
     comand, args = callback.data.split(":", maxsplit=1)
     match comand:
         case "redo":
-            logging.info(f"{callback.from_user.full_name} /gen {args}")
-            await bot.send_photo(callback.from_user.id, ai_api.generate_image(args), reply_markup=keyboard.generate([["try again", "redo:" + args]]))
+            await bot.send_photo(callback.from_user.id, ai.generate_image(args), reply_markup=keyboard.generate([["Reuse prompt", "redo:" + args], ["Prompt", "prompt:" + args]]))
+        case "prompt":
+            await bot.send_message(callback.from_user.id, args)
+        case "change_model":
+            ai.set_model(args)
+            await bot.delete_message(callback.message.chat.id, callback.message.message_id)
         case _:
             pass
 
